260f16012cf9c7b646c8c5d2b5917d1d78179b9f
Alain Serrano <alains1984@gmail.com>
2023-06-15 19:50:55 +0200

    Fixing Errors

--- library/settings.py
+++ library/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d+l3!@8$^=r+!*=(nqjqhn^iq20v_q8vs=&0=0*!aj-iuwc5*e'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
